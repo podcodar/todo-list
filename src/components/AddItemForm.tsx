e06f38cafce3169c2036cd327a380cc6b51b28ad
Guilherme barbosa <guilhermebarbo17@hotmail.com>
2022-03-29 20:11:38 -0300

    feat:create input value function

--- src/components/AddItemForm.tsx
+++ src/components/AddItemForm.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AddItemForm() {
   const domInputId = 'new-todo-input';
+  const [captureInput, setCaptureInput] = useState<string>();
+
+  const handleInputText = () => {
+    setCaptureInput('');
+  };
+
   return (
-    <form>
+    <form onSubmit={handleInputText}>
       <h2 className="label-wrapper">
         <label htmlFor={domInputId} className="label__lg">
           Lista de tarefas PodCodar
@@ -11,12 +17,18 @@ function AddItemForm() {
       </h2>
       <input
         type="text"
+        value={captureInput}
         id={domInputId}
         className="input input__lg"
         name={domInputId}
         autoComplete="off"
+        onChange={(e) => setCaptureInput(e.target.value)}
       />
-      <button type="button" className="btn btn__primary btn__lg">
+      <button
+        type="button"
+        className="btn btn__primary btn__lg"
+        onClick={handleInputText}
+      >
         Add Task
       </button>
     </form>
